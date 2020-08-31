5bf3f7d88ca7ed78d1c4c2000b167c90a3316956
wangxiaobai <qian.wang@yunding360.com>
2020-08-31 22:06:42 +0800

    feat: iconfont feat: 公共组件开发

--- src/application/Home/index.js
+++ src/application/Home/index.js
@@ -1,13 +1,49 @@
 import React from 'react'
 import { renderRoutes } from 'react-router-config'
+import { Top, Tab, TabItem } from './style'
+import { NavLink } from 'react-router-dom'
 
 const Home = props => {
   const { route } = props
+  const Tabs = [
+    {
+      id: 1,
+      path: '/recommend',
+      name: '推荐'
+    },
+    {
+      id: 2,
+      path: '/singers',
+      name: '歌手'
+    },
+    {
+      id: 3,
+      path: '/rank',
+      name: '排行榜'
+    }
+  ]
   return (
-    <div>
-      Home
+    <>
+      <Top>
+        <span className="iconfont menu">&#xe65c;</span>
+        <span className="title">WebApp</span>
+        <span className="iconfont search">&#xe62b;</span>
+      </Top>
+      <Tab>
+        {
+          Tabs.map(tab => {
+            return (
+              <NavLink key={tab.id} to={tab.path} activeClassName="selected">
+                <TabItem>
+                  <span> {tab.name} </span>
+                </TabItem>
+              </NavLink>
+            )
+          })
+        }
+      </Tab>
       { renderRoutes(route.routes) }
-    </div>
+    </>
   )
 }
 
