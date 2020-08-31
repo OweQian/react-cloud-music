f1541271495f74f768417ae67c3411fab85c8bb6
wangxiaobai <qian.wang@yunding360.com>
2020-08-31 23:58:43 +0800

    feat: redux 准备

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { GlobalStyle } from './style'
+import { Provider } from 'react-redux'
 import { IconStyle } from './assets/iconfont/iconfont'
 import { renderRoutes } from 'react-router-config'
 import routes from './routes/index'
+import store from './store/index'
 import { HashRouter } from 'react-router-dom'
 
 function App() {
   return (
-    <HashRouter>
-      <GlobalStyle/>
-      <IconStyle/>
-      { renderRoutes(routes) }
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <GlobalStyle/>
+        <IconStyle/>
+        { renderRoutes(routes) }
+      </HashRouter>
+    </Provider>
   );
 }
 
